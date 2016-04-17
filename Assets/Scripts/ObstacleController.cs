17c7c7bfe8b845e773f63b15e65f141559cf895e
Seiyaryu <gaugedamien@gmail.com>
2016-04-17 14:30:37 +0200

    Update : completed the ObstacleController script.

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -2,5 +2,22 @@
 using System.Collections;
 
 public class ObstacleController : MonoBehaviour {
+    Rigidbody2D rb;
+    
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Setup(float height, float length, Vector2 position, float speed)
+    {
+        transform.position = position;
+        Vector3 scale = transform.localScale;
+        scale.x = length;
+        scale.y = height;
+        transform.localScale = scale;
+        rb.velocity = Vector3.left * speed;
+    }
+
 
 }
