9148317f23120503376761f9fbd6fc02ffd933aa
Seiyaryu <gaugedamien@gmail.com>
2016-04-17 20:39:03 +0200

    Kill player on collide, refactored player controller and game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,10 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    public GameObject player;
+    public GameObject spawner;
+
 	public static GameManager instance = null;
-	public Transform[] rails;
-	public GameObject playerPrefab;
-	private GameObject player;
-	public int maxRailNumber = 3;
-	private int currentRail = 2;
 
 	// Use this for initialization
 	void Awake ()
@@ -24,35 +22,12 @@ public class GameManager : MonoBehaviour {
 
 	public void StartGame()
 	{
-		player = Instantiate (playerPrefab, rails[currentRail].position, Quaternion.identity) as GameObject;
-	}
+		player.SetActive(true);
+        spawner.SetActive(true);
+    }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-
-	// Update is called once per frame
-	void Update () 
-	{
-		if (player) {
-			if (Input.GetButtonDown("Up"))
-			{
-				int targetRail = Mathf.Max (currentRail - 1, (5 - maxRailNumber) / 2);
-				player.transform.position = rails [targetRail].position;
-				currentRail = targetRail;
-			}
-			if (Input.GetButtonDown("Down")) 
-			{
-				int targetRail = Mathf.Min (currentRail + 1, 3 + (maxRailNumber - 3)/2);
-				player.transform.position = rails [targetRail].position;
-				currentRail = targetRail;
-			}
-			if (Input.GetButtonDown ("Swap"))
-			{
-				player.GetComponent<PlayerController> ().SwapColor ();
-			}
-		}
-
-	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,43 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	private SpriteRenderer spriteRenderer;
+    private CircleCollider2D circleCollider;
+    public Transform[] rails;
+    private int currentRail = 2;
+    public int maxRailNumber = 3;
+    private bool collided = false;
+    private bool goUp = false;
+    private bool goDown = false;
 
 	// Use this for initialization
 	void Awake ()
 	{
+        transform.position = rails[currentRail].position;
 		spriteRenderer = GetComponent<SpriteRenderer> ();
+        circleCollider = GetComponent<CircleCollider2D> ();
 		spriteRenderer.color = Color.black;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate () {
+	    if(!collided && spriteRenderer.color == Color.white)
+        {
+            Destroy(gameObject);
+        }
+        if (goUp)
+        {
+            SetRail(currentRail - 1);
+            goUp = false;
+        }
+        else if (goDown)
+        {
+            SetRail(currentRail + 1);
+            goDown = false;
+        }
+        else
+        {
+            collided = false;
+        }
+    }
 
 	public void SwapColor ()
 	{
@@ -25,4 +50,45 @@ public class PlayerController : MonoBehaviour {
 			spriteRenderer.color = Color.black;
 		}
 	}
+
+    public void Update ()
+    {
+        if (Input.GetButtonDown("Up"))
+        {
+            goUp = true;
+        }
+        if (Input.GetButtonDown("Down"))
+        {
+            goDown = true;
+        }
+        if (Input.GetButtonDown("Swap"))
+        {
+            SwapColor();
+        }
+    }
+
+    void SetRail (int railIndex)
+    {
+        int targetRail = Mathf.Min(Mathf.Max(railIndex, (5 - maxRailNumber) / 2), 3 + (maxRailNumber - 3) / 2);
+        transform.position = rails[targetRail].position;
+        currentRail = targetRail;
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Obstacle"))
+        {
+            collided = true;
+            if (spriteRenderer.color == Color.black)
+            {
+                BoxCollider2D box = collider.gameObject.GetComponent<BoxCollider2D>();
+                Vector2 left = transform.position + circleCollider.radius * Vector3.left;
+                Vector2 right = transform.position + circleCollider.radius * Vector3.right;
+                if (box.OverlapPoint(left) && box.OverlapPoint(right))
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
 }
