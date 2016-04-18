cb9c91255e9f65a8573ca5664d0e4df3f8a17f0b
Seiyaryu <gaugedamien@gmail.com>
2016-04-18 15:42:37 +0200

    Levels can be loaded from csv put in Resources/Levels folder.

--- Assets/Scripts/SpawnerController.cs
+++ Assets/Scripts/SpawnerController.cs
@@ -16,6 +16,16 @@ public class SpawnParameters
     }
 }
 
+public class Level
+{
+    public SpawnParameters[] spawns;
+
+    public Level(SpawnParameters[] spawnParameters)
+    {
+        spawns = spawnParameters;
+    }
+}
+
 public class SpawnerController : MonoBehaviour {
 
     public ObstacleController obstaclePrefab;
@@ -24,29 +34,57 @@ public class SpawnerController : MonoBehaviour {
 
     public float speed;
 
-    private SpawnParameters[] spawnParameters;
+    private Level[] levels;
+    private int levelIndex = 0;
     private int spawnIndex = 0;
 
     [HideInInspector]
     public float time = 0.0f;
 
-	// Use this for initialization
-	void Awake ()
-    {
-        //Test code
-        spawnParameters =  new SpawnParameters[3];
-        spawnParameters[0] = new SpawnParameters(1f, 2, 3f);
-        spawnParameters[1] = new SpawnParameters(1f, 1, 4f);
-        spawnParameters[2] = new SpawnParameters(7f, 3, 6f);
-    }
+    private char lineSeparator = '\n';
+    private char fieldSeparator = ',';
 	
+    void Start ()
+    {
+        LoadLevels();
+    }
+
+    void LoadLevels ()
+    {
+        TextAsset[] levelAssets = Resources.LoadAll<TextAsset>("Levels");
+
+        levels = new Level[levelAssets.Length];
+
+        for (int levelIndex = 0; levelIndex < levelAssets.Length; levelIndex++)
+        {
+            string[] spawns = levelAssets[levelIndex].text.Split(lineSeparator);
+
+            SpawnParameters[] levelParameters = new SpawnParameters[spawns.Length];
+
+            for (int lineIndex = 0; lineIndex < spawns.Length; lineIndex++)
+            {
+                string[] spawnParameters = spawns[lineIndex].Split(fieldSeparator);
+                if(spawnParameters.Length >= 3)
+                {
+                    float spawnTime = float.Parse(spawnParameters[0]);
+                    int railIndex = int.Parse(spawnParameters[1]);
+                    float railLength = float.Parse(spawnParameters[2]);
+
+                    levelParameters[lineIndex] = new SpawnParameters(spawnTime, railIndex, railLength);
+                }
+            }
+
+            levels[levelIndex] = new Level(levelParameters);
+        }
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
         time += Time.deltaTime;
-        while (spawnIndex < spawnParameters.Length && spawnParameters[spawnIndex].spawnTime <= time)
+        while (spawnIndex < levels[levelIndex].spawns.Length && levels[levelIndex].spawns[spawnIndex].spawnTime <= time)
         {
-            int railIndex = spawnParameters[spawnIndex].railIndex;
+            int railIndex = levels[levelIndex].spawns[spawnIndex].railIndex;
             float height;
             float offset;
             if (railIndex == 0)
@@ -66,9 +104,9 @@ public class SpawnerController : MonoBehaviour {
                 height = 0.5f * (toPrevious + toNext);
                 offset = 0.5f * (toPrevious - toNext);
             }
-            Vector3 obstaclePosition = new Vector3(transform.position.x + spawnParameters[spawnIndex].railLength * 0.5f, rails[railIndex].position.y + offset, 0f);
+            Vector3 obstaclePosition = new Vector3(transform.position.x + levels[levelIndex].spawns[spawnIndex].railLength * 0.5f, rails[railIndex].position.y + offset, 0f);
             ObstacleController obstacle = (ObstacleController)Instantiate(obstaclePrefab, obstaclePosition, Quaternion.identity);
-            obstacle.Setup(height, spawnParameters[spawnIndex].railLength, speed);
+            obstacle.Setup(height, levels[levelIndex].spawns[spawnIndex].railLength, speed);
             spawnIndex++;
         }
 	}
