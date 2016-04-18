0366fcbb5caba9e645592ac053e908773695835a
Seiyaryu <gaugedamien@gmail.com>
2016-04-18 10:50:09 +0200

    Created the CarmeraController script.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform rails;
+    private Camera mainCamera;
+
+    void Start ()
+    {
+        mainCamera = GetComponent<Camera> ();
+    }
+	
+	void Update () {
+        Vector3 position = transform.position;
+        position.x = rails.transform.position.x + mainCamera.aspect * mainCamera.orthographicSize - 2.5f;
+        transform.position = position;
+	}
+}
