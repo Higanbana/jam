2966fa6a1c0563930ebe1a8ae3c984deec6370b6
Seiyaryu <gaugedamien@gmail.com>
2018-05-08 20:58:03 +0200

    Level Icons done.

--- Assets/Scripts/ShiftScreenController.cs
+++ Assets/Scripts/ShiftScreenController.cs
@@ -12,9 +12,13 @@ public class ShiftScreenController : MonoBehaviour {
     public RectTransform levelIconPrefab;
     public LevelScreenController levelScreenPrefab;
 
+    public Color defaultColor;
+    public Color highlightColor;
+
     private Camera mainCamera;
 
-    private int screenCount;
+    private int levelCount;
+    private int levelIndex;
 
     public float fallbackIntensity = 0.1f;
     public float snapThreshold = 0.1f;
@@ -23,7 +27,9 @@ public class ShiftScreenController : MonoBehaviour {
     {
         scrollRect = GetComponent<ScrollRect>();
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
-        screenCount = levelSelectorPanel.childCount;
+
+        levelIndex = 0;
+        levelIconsPanel.GetChild(levelIndex).GetComponentInChildren<Image>().color = highlightColor;
     }
     
     public void AddLevel(Level level, int highscore)
@@ -36,22 +42,47 @@ public class ShiftScreenController : MonoBehaviour {
         RectTransform levelIcon = (RectTransform)Instantiate(levelIconPrefab, Vector3.zero, Quaternion.identity);
         levelIcon.SetParent(levelIconsPanel, false);
         levelIcon.localPosition = new Vector3(levelIcon.rect.width * (levelIconsPanel.childCount - 0.5f), 0f, 0f);
-        levelIconsPanel.offsetMin = new Vector2(-0.5f * levelIcon.rect.width * levelIconsPanel.childCount, 0f);
+        levelIcon.GetComponentInChildren<Image>().color = defaultColor;
+        levelIconsPanel.anchoredPosition = new Vector2(-0.5f * levelIcon.rect.width * levelIconsPanel.childCount, 0f);
+
+        levelCount++;
     }
 
     public void Increment()
     {
         levelSelectorPanel.position += Vector3.left * mainCamera.pixelWidth;
+        HighlightSelectedLevel();
     }
 
     public void Decrement()
     {
         levelSelectorPanel.position += Vector3.right * mainCamera.pixelWidth;
+        HighlightSelectedLevel();
+    }
+
+    int CurrentLevelIndex
+    {
+        get
+        {
+            return Mathf.Min(Mathf.Max(Mathf.RoundToInt( - levelSelectorPanel.position.x / mainCamera.pixelWidth), 0), levelCount - 1);
+        }
+    }
+
+    void HighlightSelectedLevel()
+    {
+        int oldLevelIndex = levelIndex;
+        int newLevelIndex = CurrentLevelIndex;
+        if (oldLevelIndex != newLevelIndex)
+        {
+            levelIconsPanel.GetChild(oldLevelIndex).GetComponentInChildren<Image>().color = defaultColor;
+            levelIconsPanel.GetChild(newLevelIndex).GetComponentInChildren<Image>().color = highlightColor;
+            levelIndex = newLevelIndex;
+        }
     }
 
     void Update ()
     {
-        float target = Mathf.Min(Mathf.Max(Mathf.RoundToInt(levelSelectorPanel.position.x / mainCamera.pixelWidth), 1 - screenCount), 0) * mainCamera.pixelWidth;
+        float target = - CurrentLevelIndex * mainCamera.pixelWidth;
         if(Mathf.Abs(target - levelSelectorPanel.position.x) < snapThreshold * mainCamera.pixelWidth)
         {
             Vector3 panelPosition = levelSelectorPanel.position;
